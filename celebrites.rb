559a843d405ea6e88cc0b30a7489b8587e148795
hadjadiallo <hadjadiallo0101@gmail.com>
2018-06-20 20:19:18 +0000

    automatically backed up by learn

---
+++ celebrites.rb
@@ -0,0 +1,22 @@
+fav_celebrities = {}
+fav_celebrities["emma"]="ed sheeran"
+fav_celebrities{"shay"=> "anthony bourdain"
+"grace"=> "shawn mendes"
+"hadja" => "rihanna"
+"seema"=> "Allison kim"
+"anu"=> "the queen"
+"jesse"=> "Big time rush"
+"cassidy"=> "hanna montana"
+
+
+
+
+}
+fav_celebrities ["emma"]= "ed sheeran"
+ students_name= fav_celebrities
+ celeb = fav_celebrities.value
+ 
+ 
+ fav_celebrities.each do |students_name,celeb|
+puts"#{ students_name}'s favorite celebrity is #{caleb}"
+end
